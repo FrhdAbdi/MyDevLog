0062cf8dbea144885077cda3701af63d6f2674e0
Farhad10Abdi <Farhad10abdi@gmail.com>
2025-09-11 10:04:20 +0330

    Add light mode theme and toggle functionality

---
+++ MyDevLog/wwwroot/js/theme.js
@@ -0,0 +1,11 @@
+﻿export function applyTheme(theme) {
+    document.documentElement.setAttribute('data-theme', theme);
+}
+
+export function getInitialTheme() {
+    return localStorage.getItem('theme') || 'dark';
+}
+
+export function setThemeInStorage(theme) {
+    localStorage.setItem('theme', theme);
+}
